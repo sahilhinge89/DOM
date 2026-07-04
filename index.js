e0e96541839641cb3381f179193e78ee4cb8437c
Sahil Hinge <sahilhinge89@gmail.com>
2026-07-04 14:45:01 +0530

    Create types of functions

---
+++ index.js
@@ -0,0 +1,32 @@
+//console.log("kya haal chal")
+
+function run(){
+    console.log("running");
+}
+run();
+
+let stand = function walk(){
+    console.log('walking')
+};
+stand();
+let jump = stand;
+jump ();
+
+function sum (a,b){
+    let total = 0;
+    for (let value of arguments)
+        total = total + value;
+    return total;
+}
+
+console.log(sum(1,3,5))
+
+let person ={
+    fName : 'Sahil',
+    lName : 'Hinge'
+}
+
+function fullName(){
+    return `${person.fName} ${person.lName}`
+}
+console.log(fullName())
